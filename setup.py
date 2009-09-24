bad62a0eefbf543b4a942ef6cc273feae0a529db
Jonas Bernoulli <jonas@bernoul.li>
2009-09-24 23:14:04 +0200

    use distutils

---
+++ setup.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2009  Jonas Bernoulli <jonas@bernoul.li>
+
+from distutils.core import setup
+
+setup(name='etools',
+      version='9999',
+      description='Misc portage utilities',
+      author='Jonas Bernoulli',
+      author_email='jonas@bernoul.li',
+      url='http://github.com/tarsius/etools/',
+      package_dir={'': 'lib'},
+      py_modules=['etools'])
